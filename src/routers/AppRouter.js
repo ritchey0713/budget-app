9e643301c3214fb96f3aca356ee967d141e8e5ee
ritchey0713 <ritchey0713@gmail.com>
2018-09-30 18:03:43 -0400

    add help page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 
-const HelpPage = () => (
-  <div>
-    This is from my help component
-  </div>
-);
 
 const NotFoundPage = () => (
   <div>
