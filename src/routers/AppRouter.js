0747bebd046eb1fe5683c906b99cd3cc5db05be5
ritchey0713 <ritchey0713@gmail.com>
2018-09-30 18:07:51 -0400

    add header page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <h1>Budget App</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink> |
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink> |
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> |
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink> |
-  </header>
-);
+
 const AppRouter = () => (
 <BrowserRouter>
     <div>
