ae663fe56733cc0c55286f534ac544c716022f69
ritchey0713 <ritchey0713@gmail.com>
2018-09-30 17:56:57 -0400

    add dashboard component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my dashboard component
-  </div>
-);
+
 
 const AddExpensePage = () => (
   <div>
