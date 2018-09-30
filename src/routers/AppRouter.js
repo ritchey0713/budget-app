0d9282b9c5f2914762fed9cd18166bfb1ed5642e
ritchey0713 <ritchey0713@gmail.com>
2018-09-30 18:02:23 -0400

    add edit page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 
-
-
-
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-);
-
 const HelpPage = () => (
   <div>
     This is from my help component
