184f45e349f69d79a43856818662b420d0407993
ritchey0713 <ritchey0713@gmail.com>
2018-09-30 18:05:42 -0400

    add 404 page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-
-
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go home</Link>
-  </div>
-);
-
 const Header = () => (
   <header>
     <h1>Budget App</h1>
