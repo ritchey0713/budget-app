f2a8e1eb88b16b29d6b7f42e8edf500ccf33826d
ritchey0713 <ritchey0713@gmail.com>
2018-09-30 23:50:32 -0400

    change edit to be dynamic

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-
+import AddExpensePage from '../components/AddExpensePage.js'
+import EditExpensePage from '../components/EditExpensePage.js'
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js'
+import Header from '../components/Header.js'
+import HelpPage from '../components/HelpPage.js'
+import NotFoundPage from '../components/NotFoundPage.js'
 
 const AppRouter = () => (
 <BrowserRouter>
@@ -9,7 +14,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
