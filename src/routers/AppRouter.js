675481299d4edee7b10486b9e69cf89268f46943
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-01 19:58:16 -0400

    fix issues

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ExpenseDashboardPage from './../components/ExpenseDashboardPage';
 import AddExpensePage from './../components/AddExpensePage';
@@ -7,15 +7,7 @@ import EditExpensePage from './../components/EditExpensePage';
 import HelpPage from './../components/HelpPage';
 import NotFoundPage from './../components/NotFound';
 import Header from './../components/Header'
-=======
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import AddExpensePage from '../components/AddExpensePage.js'
-import EditExpensePage from '../components/EditExpensePage.js'
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js'
-import Header from '../components/Header.js'
-import HelpPage from '../components/HelpPage.js'
-import NotFoundPage from '../components/NotFoundPage.js'
->>>>>>> f2a8e1eb88b16b29d6b7f42e8edf500ccf33826d
+
 
 const AppRouter = () => (
 <BrowserRouter>
