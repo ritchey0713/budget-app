328a1b8879b77460840e82b6053cfe60261b0827
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-03 14:41:42 -0400

    build out fake store

---
+++ src/playground/redux-budget.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+
+const demoState = {
+  expenses: [{
+    id: "234234234",
+    description: "Rent",
+    note: "Final payment for apartment",
+    amount: 54500,
+    createdAt: 0
+  }]
+}
