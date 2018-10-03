437ad432a860260a14992aa3aa63ec6e9f129c5e
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-03 15:44:16 -0400

    add reducers

--- src/playground/redux-budget.js
+++ src/playground/redux-budget.js
@@ -1,5 +1,37 @@
 import { createStore, combineReducers } from 'redux';
 
+const expensesDefaultState = []
+
+const expensesReducer = (state = expensesDefaultState, action) => {
+  switch(action.type) {
+    default:
+    return state
+  }
+}
+
+const filtersDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+}
+
+const filtersReducer = (state = filtersDefaultState, action) => {
+  switch(action.type){
+    default: 
+    return state
+  }
+}
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+)
+
+console.log(store.getState())
+
 const demoState = {
   expenses: [{
     id: "234234234",
@@ -7,5 +39,11 @@ const demoState = {
     note: "Final payment for apartment",
     amount: 54500,
     createdAt: 0
-  }]
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: "amount", // date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
 }
