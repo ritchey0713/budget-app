052f5cf408d8859d88d9aa4b2f5f5e0f63218f43
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-03 16:26:34 -0400

    remove action set up

--- src/playground/redux-budget.js
+++ src/playground/redux-budget.js
@@ -1,9 +1,41 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid'
+
+const addExpense = (
+  { description = "", 
+    note = "", 
+    amount = 0, 
+    createdAt = 0 
+  } = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+
+  }
+})
+
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id
+})
 
 const expensesDefaultState = []
 
 const expensesReducer = (state = expensesDefaultState, action) => {
   switch(action.type) {
+    case "ADD_EXPENSE":
+      return [
+        ...state,
+        action.expense
+      ]
+    case "REMOVE_EXPENSE": 
+      return state.filter(({ id }) => id !== action.id
+      ) 
+
     default:
     return state
   }
@@ -30,8 +62,17 @@ const store = createStore(
   })
 )
 
-console.log(store.getState())
+store.subscribe(() => {
+  console.log(store.getState())
+})
 
+const expense1 = store.dispatch(addExpense({ description: "Rent", amount: 100 }));
+store.dispatch(addExpense({ description: "Coffee", amount: 300 }));
+store.dispatch(addExpense({ description: "Keyboard", amount: 10000 }));
+
+store.dispatch(removeExpense({ id: expense1.expense.id }))
+
+console.log(expense1)
 const demoState = {
   expenses: [{
     id: "234234234",
