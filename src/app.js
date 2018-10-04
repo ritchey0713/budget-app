d72358dd3304237e980a37797d877ae6b312d6f7
ritchey0713 <ritchey0713@gmail.com>
2018-10-04 19:43:15 -0400

    add mapProps

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
@@ -13,8 +14,18 @@ store.dispatch(addExpense({ description: "Water Bill", amount: 1000, createdAt: 
 store.dispatch(addExpense({ description: "Gas Bill", amount: 1000, createdAt: -100 }));
 store.dispatch(setTextFilter("gas"))
 
+setTimeout(() => {
+  store.dispatch(setTextFilter("water"))
+}, 3000)
+
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter  />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const ExpenseList = (props) => (
+  <div>
+    <h1> Expense List! </h1>
+    {props.filters.text}
+    {props.expenses.length}
+  </div>
+)
+
+// const ConnectedExpenseList = connect((state) => {
+//   return {
+//     expenses: state.expenses
+//   }
+// })(ExpenseList);
+
+// export default ConnectedExpenseList
+
+const mapStateToProps = (state) => {
+  return {
+    expenses: state.expenses,
+    filters: state.filters
+  }
+}
+
+export default connect(mapStateToProps)(ExpenseList)
