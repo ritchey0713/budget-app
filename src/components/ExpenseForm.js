cb04fa2d39633bb59636efb233a9f2a42d2653e3
ritchey0713 <ritchey0713@gmail.com>
2018-10-05 17:41:02 -0400

    add form logic

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
 
 class ExpenseForm extends Component {
+  state = {
+    description: '',
+    note: ''
+  }
+  onDescriptionChange = (e) => {
+    const description = e.target.value
+    this.setState(() => ({ description }))
+  }
+
+  onNoteChange = (e) => {
+    const note = e.target.value
+    this.setState(() => ({ note }))
+  }
+
   render(){
     return(
       <div>
-        Expense form
+      <form>
+        <input
+          type="text"
+          placeholder="Description"
+          autoFocus
+          value={this.state.description}
+          onChange={this.onDescriptionChange}
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+        />
+        <textarea
+          placeholder="Add a note for the expense (OPTIONAL)"
+          value={this.state.note}
+          onChange={this.onNoteChange}
+          >
+        </textarea>
+        <button>Add Expense</button>
+          </form>
       </div>
     )
   }
