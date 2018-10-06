9ce383d545c2de7dfe2cfee737fcd05c2d46d24d
ritchey0713 <ritchey0713@gmail.com>
2018-10-06 17:11:29 -0400

    add link to expense title

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { removeExpense } from '../actions/expenses'
-
+import { Link } from 'react-router-dom';
 const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
     <ul>
-    <li><h3> {description}</h3> </li>
+    <Link to={`/edit/${id}`} >
+      <li><h3> {description}</h3> </li>
+    </Link>
     <li> {amount} </li>
     <li> {createdAt} </li>
     <button onClick={() => {
