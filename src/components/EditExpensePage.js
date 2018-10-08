4c8742d32ceb28cc0295ab75f416d8ff7a94bbd1
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-08 12:08:20 -0400

    upgrade yarn

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
-console.log(props)
   return(
 
       <div>
-        Editing expense with id of {props.match.params.id}
+        <ExpenseForm
+          expense={props.expense}
+          onSubmit={(expense) => {
+            props.dispatch(editExpense(props.match.params.id, expense))
+            props.history.push('/')
+          }} />
       </div>
 
   )
 }
 
-export default EditExpensePage
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => (expense.id === props.match.params.id))
+  }
+}
+
+export default connect(mapStateToProps)(EditExpensePage)
 
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -9,14 +9,18 @@ const now = moment();
 console.log(now.format("MMM Do, YYYY"))
 
 class ExpenseForm extends Component {
-  state = {
-    description: '',
-    note: '',
-    amount: "",
-    createdAt: moment(),
-    calanderFocused: false,
-    error: ""
+  constructor(props){
+    super(props)
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount /100).toString() : "",
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calanderFocused: false,
+      error: ""
+    }
   }
+    
   onDescriptionChange = (e) => {
     const description = e.target.value
     this.setState(() => ({ description }))
