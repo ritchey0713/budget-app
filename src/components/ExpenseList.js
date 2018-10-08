0802cab7d4755bf21a89b1e2ad5addd16694a109
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-08 15:48:12 -0400

    no changes

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
+
 const ExpenseList = (props) => (
   <div>
     <h1> Expense List! </h1>
