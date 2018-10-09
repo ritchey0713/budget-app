74c92d8d3f3a63cd3a617ba641d3e045603d326b
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-09 18:26:15 -0400

    ADD FILTERS TESTS

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,1 +1,53 @@
-import { setEndDate, setStartDates } from '../../actions/filters';
+import { setEndDate, 
+         setStartDate, 
+         sortByAmount, 
+         sortByDate, 
+         setTextFilter } from '../../actions/filters';
+import moment from 'moment'
+
+test("should generate set start date", () => {
+  const action = setStartDate(moment(0))
+  expect(action).toEqual({
+    type: "SET_START_DATE",
+    date: moment(0)
+  })
+})
+
+test("should generate set end date", () => {
+  const action = setEndDate(moment(0))
+  expect(action).toEqual({
+    type: "SET_END_DATE",
+    date: moment(0)
+  })
+})
+
+test("should sort by amount", () => {
+  const action = sortByAmount()
+  expect(action).toEqual({
+    type: "SORT_BY_AMOUNT"
+  })
+})
+
+test("should sort by date", () => {
+  const action = sortByDate()
+  expect(action).toEqual({
+    type: "SORT_BY_DATE"
+  })
+})
+
+test("should sort by text value", () => {
+  const text = "Test string"
+  const action = setTextFilter(text)
+  expect(action).toEqual({
+    type: "SET_TEXT",
+    text: text
+  })
+})
+
+test('should return all expenses with no arguments', () => {
+  const action = setTextFilter()
+  expect(action).toEqual({
+    type: "SET_TEXT",
+    text: ""
+  })
+})
