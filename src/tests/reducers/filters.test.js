296dc468bfb20fc1f81920fe01cb0f65c3cc9068
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-10 09:02:03 -0400

    add default state test

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,1 +1,13 @@
 import filtersReducer from '../../reducers/filters';
+import moment from 'moment' 
+
+test("should set up default values", () => {
+  const state = filtersReducer(undefined, { type: "@@INIT" })
+  expect(state).toEqual({
+    text: "",
+    sortBy: "date",
+    startDate: moment().startOf("month"),
+    endDate: moment().endOf("month")
+
+  })
+})
