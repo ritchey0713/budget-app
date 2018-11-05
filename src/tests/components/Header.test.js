6460a06c14c253a4a538c12a3b27eb063114448c
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-04 21:18:32 -0500

    create snapshot

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,4 +5,5 @@ import Header from '../../components/Header'
 test('should render header correctly', () => {
   const renderer = new ReactShallowRenderer()
   renderer.render(<Header />)
-})
+  expect(renderer.getRenderOutput()).toMatchSnapshot()
+}) 
