e13d07f80ad48923fb106041c14e4ef6356b5310
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-05 22:58:46 -0500

    create 2nd test

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import ExpenseForm from './../../components/ExpenseForm';
+import expenses from '../fixture/expenses'
+
+test('should render form correctly', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('should render expense form with data', () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>)
+  expect(wrapper).toMatchSnapshot()
+})
