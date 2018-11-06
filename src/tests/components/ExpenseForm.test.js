7695e1942ca08e33a7a1d3d1ba1950e1775dc51e
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-06 15:53:30 -0500

    add new test

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,13 @@ test('should render expense form with data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>)
   expect(wrapper).toMatchSnapshot()
 })
+
+test('should render error for bad submit', () => {
+  const wrapper = shallow(<ExpenseForm/>)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => { }
+  })
+  expect(wrapper.state('error').length).toBeGreaterThan(0)
+  expect(wrapper).toMatchSnapshot()
+})
