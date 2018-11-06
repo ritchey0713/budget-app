44fb6fa8af78a668b10834a8e57afcea3060f065
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-05 22:09:08 -0500

    upgrade yarn

--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { ExpenseList } from './../../components/ExpenseList';
-import expenses from '../fixtures/expenses'
+import expenses from '../fixture/expenses'
 
 test('should render ExpenseList with expenses', () => {
   const wrapper = shallow(<ExpenseList expenses={expenses} />)
