18f22506f309a291a74e1321974bcb5b54f52f0d
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-06 20:10:07 -0500

    create tests

---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import expenses from '../fixture/expenses'
+import { EditExpensePage } from './../../components/EditExpensePage';
+
+
+
+let editExpense, removeExpense, history, wrapper 
+
+beforeEach(() => {
+  editExpense = jest.fn()
+  removeExpense = jest.fn()
+   history = { push: jest.fn() }
+   wrapper = shallow(
+   <EditExpensePage 
+      editExpense={editExpense} 
+      removeExpense={removeExpense} 
+      history={history} 
+      expense={expenses[0]}/>
+    )
+})
+
+test('should render editExpensePage', () => {
+  expect(wrapper).toMatchSnapshot()
+})
+
+test("should handle onSubmit", () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0])
+  expect(history.push).toHaveBeenLastCalledWith('/')
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0])
+})
+
+test("should remove using onClick", () => {
+  wrapper.find('button').simulate('click')
+  expect(history.push).toHaveBeenLastCalledWith('/')
+  expect(removeExpense).toHaveBeenLastCalledWith({id: expenses[0].id})
+})
